3dc4bd49b4f21f7e676b389a59f5173871a6ee1e
rudi hinds <rudihinds@gmail.com>
2019-05-02 10:04:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,17 +21,17 @@ def take_a_number(katz_deli, name)
 end
 	
 
-# // NOW SERVING
+// NOW SERVING
 
-# function nowServing(katzDeliLine){
-# 	if(katzDeliLine.length === 0){
-# 	  return "There is nobody waiting to be served!"
+ nowServing(katzDeliLine){
+	if(katzDeliLine.length === 0){
+	  return "There is nobody waiting to be served!"
 
-# } else {
-# 		return "Currently serving " + "" + katzDeliLine.shift() + ".";
+} else {
+		return "Currently serving " + "" + katzDeliLine.shift() + ".";
 
-#   }
-# }
+  }
+}
 
 
 # // LINE FUNCTION
