957db9713107470394a503676cae60508d8f2ef7
rudi hinds <rudihinds@gmail.com>
2019-05-02 09:36:44 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,9 +9,9 @@ def line(q)
     return "The line is currently empty."
   else q.each do | person |
     position += 1
-    newArr.push("#{position}. #{person}")
+    katz_deli.push("#{position}. #{person}")
   end
-  puts " The line is currently: #{newArr.join(', ')}"
+  puts " The line is currently: #{katz_deli.join(', ')}"
 end
 end
 
